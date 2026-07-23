1faa7e8262c3662a28c630f7ec389552e0edecb9
NatYarm <yarmnat7@gmail.com>
2026-07-23 22:32:58 +0200

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +8,57 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    rules: {
+      // Formatting / code style
+
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+      ],
+
+      // React / JSX
+      'react/react-in-jsx-scope': 'off',
+      // 'react/function-component-definition': ['error'],
+      'react/jsx-props-no-spreading': 'off',
+
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': ['error'],
+
+      // Safety
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-param-reassign': [
+        'error',
+        { props: true, ignorePropertyModificationsFor: ['state', 'self'] },
+      ],
+
+      //  import order
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            { pattern: 'react', group: 'external', position: 'before' },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
