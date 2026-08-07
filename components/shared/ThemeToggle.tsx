c1c8e26e1496134852140677b6b840584308aa84
NatYarm <yarmnat7@gmail.com>
2026-08-07 13:54:50 +0200

    modify theme-toggle

--- components/shared/ThemeToggle.tsx
+++ components/shared/ThemeToggle.tsx
@@ -1,39 +1,25 @@
 'use client';
 
-import { Moon, Sun } from 'lucide-react';
+import { SunMoon } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
+
+  const handleThemeToggle = () => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="group rounded-full bg-muted w-10 h-10  hover:bg-primary/80 dark:hover:bg-primary/80"
-        >
-          <Sun className="h-5 w-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 group-hover:text-foreground group-hover:fill-foreground" />
-          <Moon className="absolute h-6 w-6 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 group-hover:text-foreground group-hover:fill-foreground" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="ghost"
+      className="group h-10 w-10 rounded-full bg-muted hover:bg-primary/80 dark:hover:bg-primary/80"
+      onClick={handleThemeToggle}
+      aria-label="Toggle theme"
+    >
+      <SunMoon className="h-5 w-5 transition-all group-hover:text-foreground group-hover:fill-foreground" />
+    </Button>
   );
 }
